77d36e8a94131d7e11519050b0308007669ffaac
vurses <vurses@qq.com>
2024-10-08 22:16:29 +0800

    complete the general upload process

--- picco.js
+++ picco.js
@@ -6,6 +6,7 @@ const commentBox = document
 const richTextarea = commentBox.richTextarea;
 const picturesUpload = commentBox.picturesUpload;
 const inputarea = richTextarea.brt.editor;
+
 inputarea.addEventListener("paste", async (event) => {
   // 获取剪贴板中的项目
   const items = (event.clipboardData || navigator.clipboardData).items;
@@ -14,25 +15,29 @@ inputarea.addEventListener("paste", async (event) => {
   for (let i = 0; i < items.length; i++) {
     // 检查是否是文本类型
     if (items[i].kind === "string") {
-      // 读取文本数据
-      const text = await getStringFromItem(items[i]);
-      console.log("粘贴的文本:", text);
     }
     // 检查是否是文件类型（图片）
     else if (items[i].kind === "file" && items[i].type.startsWith("image/")) {
       // 获取文件并处理图片
       const file = items[i].getAsFile();
-      const size = file.size;
-      console.log("图片大小 (bytes):", file.size); // 获取文件大小
+      // 上传图片
+      const { code, data, message, ttl } = await uploadFile(file);
+
       const base64String = await getBase64FromFile(file);
-      console.log("粘贴的图片的base64编码:", base64String);
 
-      const { width, height } = await getImageDimensions(base64String);
-      console.log("图片宽度:", width);
-      console.log("图片高度:", height);
-
+      if (code !== 0) {
+        console.warn(message);
+        return;
+      }
       // 设置图片数据
-      setPicData(width, height, size, base64String, picturesUpload);
+      setPicData(
+        data.image_width,
+        data.image_height,
+        data.img_size,
+        data.image_url,
+        base64String,
+        commentBox
+      );
       // 显示图片区
       picturesUpload.style.display = "flex";
       // 更新图片区图片dom显示,源码分析hack得此api
@@ -40,12 +45,6 @@ inputarea.addEventListener("paste", async (event) => {
     }
   }
 });
-// 获取文本数据
-function getStringFromItem(item) {
-  return new Promise((resolve) => {
-    item.getAsString((text) => resolve(text));
-  });
-}
 
 // 获取文件的base64编码
 function getBase64FromFile(file) {
@@ -56,27 +55,60 @@ function getBase64FromFile(file) {
   });
 }
 
-// 获取图片的宽度和高度
-function getImageDimensions(base64String) {
-  return new Promise((resolve) => {
-    const img = new Image();
-    img.onload = () => resolve({ width: img.width, height: img.height });
-    img.src = base64String;
-  });
-}
-
-// 将数据交给picturesUpload
-function setPicData(img_width, img_height, img_size, base64, target) {
+// 将数据交给picturesUpload和commentBox,一个用于显示一个准备上传给服务器
+function setPicData(
+  img_width,
+  img_height,
+  img_size,
+  img_src,
+  base64String,
+  commentBox
+) {
   const item = {
     data: {
       img_height,
       img_size,
       img_width,
-      img_src: "",
+      img_src,
     },
     msg: "",
-    src: base64,
+    src: base64String,
     status: 1,
   };
-  target.items.push(item);
+  commentBox.picturesUpload.items.push(item);
+  commentBox.pics.push(item.data);
+}
+
+// 获取cookie某个值
+function getCookie(name) {
+  const cookies = document.cookie.split("; ");
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].split("=");
+    const cookieName = cookie[0].trim();
+    if (cookieName === name) {
+      return decodeURIComponent(cookie[1].trim());
+    }
+  }
+  return null;
+}
+
+// 上传文件
+function uploadFile(file) {
+  const formData = new FormData();
+  formData.append("file_up", file);
+  formData.append("biz", "new_dyn");
+  formData.append("category", "daily");
+  formData.append("csrf", getCookie("bili_jct"));
+
+  // 不用显式设置 Content-Type 头，浏览器会自动设置 multipart/form-data
+  return fetch("https://api.bilibili.com/x/dynamic/feed/draw/upload_bfs", {
+    method: "POST",
+    headers: {
+      Accept: "*/*",
+    },
+    credentials: "include",
+    body: formData,
+  }).then((response) => {
+    return response.json();
+  });
 }
